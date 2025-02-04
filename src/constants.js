4e6b59db458c74b6c05f0b6bcf122c479dd723d3
TahmeedWolf <rahmantahmeed@gmail.com>
2025-02-04 22:22:47 +0600

    Fixed line endings issue

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/Java as well as other languages.
+    I've made several projects. I have also worked on projects using C and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/TahmeedWolf" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Master's in IT degree. I hung it on the wall because I'm proud of it. It have completed it from an Australian University, named Swinburne University.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. I have my own Youtube channel where I make gaming tutorials on YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently but I also like to watch my channel perform haha, <a href="https://www.youtube.com/@itahmeedofficial" target="_blank">iTahmeed</a>,`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/TahmeedWolf/Resume/blob/main/TahmeedRahman_Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at rahmantahmeed@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Hindi, Bengali (I also speak Hindi, Bengali and Nepali btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Tahmeed Rahman's portfolio, just close the tab.`,
+};
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/2D_Game_Developer_Portfolio/",
+  build: {
+    minify: "terser",
+  },
+});
